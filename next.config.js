891a95d7dce66fd50d8a0ee95040d91652a7e4da
satya sahoo <satyasahoo525@gmail.com>
2024-01-15 16:35:17 +0530

    added resume

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
 
     return config;
   },
+  output: 'export',
   compress: true,
   generateEtags: true,
   pageExtensions: ['tsx', 'mdx', 'ts'],
@@ -27,7 +28,8 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-      },{
+      },
+      {
         protocol: 'https',
         hostname: 'source.unsplash.com',
       },
